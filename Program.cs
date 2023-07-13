70ec36856dcc8660f23a8f39b001e3ab46eb3796
NikhilGhorband <127277360+NikhilGhorband@users.noreply.github.com>
2023-07-13 16:16:54 +0530

    [Nikhil]Add:Create Table

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ADODotNetDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Customer.CreateDatabase();
+            Customer.CreateTable();
+            
+        }
+    }
+}
